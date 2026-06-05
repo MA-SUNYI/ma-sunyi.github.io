06e4371aef5eab798d4693b01ed4c7fadb4307d3
孙毅 <976407825@qq.com>
2026-06-05 22:31:02 +0800

    文件修改

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11105303
+    "zoneId": 11105319
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
